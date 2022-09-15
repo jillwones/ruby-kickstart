544c36e39aacba5903dda6f81f6c8052bbb737f0
willj836 <willj836@gmail.com>
2022-09-15 14:31:43 +0100

    done 2:3, needed some hints from solved though

--- session2/3-challenge/3_array.rb
+++ session2/3-challenge/3_array.rb
@@ -7,6 +7,15 @@
 
 class String
   def every_other_char
+    answer = ""
+    each_char.with_index do |chr, index|
+      if index.odd?
+        next
+      end
+      answer << chr
+    end
+    answer
   end
-  
 end
+
+#passes all tests, again slightly different from solved but still works, had to get into from solve with each_char.with_index
