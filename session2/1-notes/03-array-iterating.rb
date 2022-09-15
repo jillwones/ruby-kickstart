7de725a453c63bb15e4eb77cb41ef350ccb61e55
willj836 <willj836@gmail.com>
2022-09-15 10:45:08 +0100

    added notes on each_cons method

--- session2/1-notes/03-array-iterating.rb
+++ session2/1-notes/03-array-iterating.rb
@@ -23,5 +23,8 @@ result # => [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]]
 # iterate over the array in groups of two
 # hitting each group as we go
 result = []
-numbers[0..5].each_cons(2) { |a, b| result << [a, b] }
+nums = (0..5).to_a #[0,1,2,3,4,5]
+nums.each_cons(2) { |a, b| result << [a, b] }
 result # => [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5]]
+
+#each_cons compares each consecutive 2 elements when passed (2), if i passed 3 for each_cons(3) then i would do { |a, b, c| result << [a, b, c]} and this would result in [[0, 1, 2], [1, 2, 3], [2, 3, 4], [3, 4, 5]] so it moves up one element at a time before executing the code block
