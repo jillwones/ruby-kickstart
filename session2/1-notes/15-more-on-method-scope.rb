b87a41dcda3d1507c67dbd277fb99bfb24f3a955
willj836 <willj836@gmail.com>
2022-09-15 11:57:07 +0100

    notes on initializing, the flow of code in the methods

--- session2/1-notes/15-more-on-method-scope.rb
+++ session2/1-notes/15-more-on-method-scope.rb
@@ -3,20 +3,20 @@
 # on their object.
 
 class Example
-  
+
   def method1
     self # => #<Example:0x00000100868f98>
     @var = 123
-    method2 
+    method2 #so with this it calls method2 which reassigns the instance variable @var to 500 so @var below is now 500 and as that is the final object in the block for method1 that is what is returned
     @var # => 500
   end
-  
+
   def method2
     self # => #<Example:0x00000100868f98 @var=123>
     @var # => 123
     @var = 500
   end
-  
+
 end
 
 Example.new.method1
