fb8fd03e3eb9420f75d07bbaea02f53bde6a7790
willj836 <willj836@gmail.com>
2022-09-15 10:20:21 +0100

    finished session 1

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,17 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-
+arr = string.split('')
+answer = []
+    arr.each_with_index do |x, i|
+        next_character = arr[i+1]
+        if x == "r" || x == "R"
+            answer.push(next_character)
+        end
+    end
+    answer.join
 end
+
+#passed all tests, my own answer (included an array tho cus wasnt sure how to do it just as a string), in the solved section it keeps it as a string
+
+#after rake 1:all they have all passed, so all good for these questions
