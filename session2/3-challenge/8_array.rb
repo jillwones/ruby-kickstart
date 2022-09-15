dd5884cfe7a82202c699312bf2e85c740d3852a0
willj836 <willj836@gmail.com>
2022-09-15 16:36:44 +0100

    completed 2:8 on my own, passes all tests but nicer answer in solved using each_cons

--- session2/3-challenge/8_array.rb
+++ session2/3-challenge/8_array.rb
@@ -6,3 +6,13 @@
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
 
+def got_three?(array)
+    array.each_with_index do |x, i|
+        if x == array[i + 1] && x == array[i + 2]
+            return true
+        end
+    end
+    false
+end
+
+#passes all tests, my own answer, no hints - nicer answer in solved using each_cons though
